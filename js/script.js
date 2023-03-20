b56696162a77cd33aafc0cee086b192ea59d0624
RuggeroCoco <ruggerococo94@gmail.com>
2023-03-20 19:45:42 +0100

    End of project

--- js/script.js
+++ js/script.js
@@ -3,29 +3,57 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-const grid = document.querySelector(".grid");
-const spownGridBtn = document.getElementById("spown-grid");
-spownGridBtn.addEventListener("click", spowngrid);
+document.getElementById("spown-grid").addEventListener("click", startGame);
 
-function spowngrid() {
-  if (document.querySelector(".grid").childElementCount > 0) {
-  } else {
-    for (let i = 1; i <= 100; i++) {
-      const currentNumber = i;
-      const newItem = generateGridItem(currentNumber);
-      newItem.addEventListener("click", handleItemClick);
-      grid.append(newItem);
-    }
+function startGame() {
+  const level = parseInt(document.querySelector("select").value);
+  let cellsNumber;
+  switch (level) {
+    case 1:
+      cellsNumber = 100;
+      break;
+    case 2:
+      cellsNumber = 81;
+      break;
+    case 3:
+      cellsNumber = 49;
+      break;
+    default:
+      cellsNumber = 100;
+  }
+  console.log(cellsNumber);
+
+  const gridElem = document.querySelector(".grid");
+  const introText = document.querySelector("h2");
+  introText.classList.add("hidden");
+  gridElem.classList.remove("hidden");
+  gridElem.innerHTML = "";
+
+  for (let i = 1; i <= cellsNumber; i++) {
+    const newElem = createGridElem(i, Math.sqrt(cellsNumber));
+    newElem.addEventListener("click", handleCellClick);
+    gridElem.append(newElem);
   }
 }
 
-function generateGridItem(text) {
-  const newSquare = document.createElement("div");
-  newSquare.classList.add("grid-item");
-  newSquare.innerHTML = `<span>${text}</span>`;
-  return newSquare;
+/**
+ * Description
+ * @param {string || number} content
+ * @param {number} cellSize
+ * @returns {object}
+ */
+
+function createGridElem(content, cellSize) {
+  const gridElem = document.createElement("div");
+  gridElem.classList.add("grid-item");
+  gridElem.style.width = `calc(100% / ${cellSize})`;
+  gridElem.style.height = `calc(100% / ${cellSize})`;
+  gridElem.innerHTML = `<span>${content}</span>`;
+  return gridElem;
 }
 
-function handleItemClick() {
-  this.classList.toggle("click");
+function handleCellClick() {
+  this.classList.add("clicked");
+  const clickedNumber = parseInt(this.querySelector("span").textContent);
+  console.log(clickedNumber);
 }
